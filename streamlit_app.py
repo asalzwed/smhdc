4e317a4efab7bc3526012a414d33969a69fb1b8c
asalzwed <salzweda@gmail.com>
2025-06-29 18:08:27 +0000

    v01

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,13 @@ import pandas as pd
 # Load data
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/smartpak_products.csv")
-    df["price"] = pd.to_numeric(df["price"], errors="coerce")
-    return df
+    df0 = pd.read_csv("data/smartpak_products.csv")
+    df0["price"] = pd.to_numeric(df0["price"], errors="coerce")
 
-df = load_data()
+    df1 = pd.read_csv("data/smartpak_ingredient_lookup.csv")
+    return df0, df1
+
+df, ingredients = load_data()
 
 st.title("🧴 Supplement My Horse Dot Com")
 
@@ -37,22 +39,33 @@ price_range = st.sidebar.slider("Price Range ($):", min_value=min_price, max_val
 # Product name search
 search = st.sidebar.text_input("Search Product Name:")
 
+# Display data
+all_ingredient = ingredients["ingredient"].dropna().unique()
+selected_ingredient = st.multiselect("Select Ingredient", options=all_ingredient, max_selections=1, default=['Chondroitin Sulfate (Bovine)'])
+id_ing = ingredients[ingredients['ingredient'].isin(selected_ingredient)].drop_duplicates()
+
 # Apply filters
 filtered_df = df[
     df["brand"].isin(selected_brands) &
     df["category_child"].isin(selected_categories) &
     df["price"].between(price_range[0], price_range[1])
+
 ]
+
+filtered_df = filtered_df.merge(id_ing[['product_id','ingredient', 'amount', 'unit']],left_on='id',right_on='product_id')
 
 if search:
     filtered_df = filtered_df[filtered_df["name"].str.contains(search, case=False, na=False)]
 
-# Display data
 st.markdown(f"### Showing {len(filtered_df)} product(s)")
-st.dataframe(filtered_df.drop(columns=['id','category_parent']).reset_index(drop=True).rename(columns={'brand':'Brand',
+st.dataframe(filtered_df.drop(columns=['id','product_id','category_parent']).reset_index(drop=True).rename(columns={'brand':'Brand',
 'category_child':'Category',
 'price':'Price ($)',
-'name':'Product Name'}),hide_index=True)
+'name':'Product Name'}),
+hide_index=True,
+column_config={
+    "url": st.column_config.LinkColumn("Link",display_text="🔗",width="small")})
+
 
 # Option to download filtered data
 st.download_button("📥 Download Filtered CSV", filtered_df.to_csv(index=False), file_name="filtered_smartpak_products.csv")
